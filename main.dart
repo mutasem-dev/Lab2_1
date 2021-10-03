0eb583f4e2aae5981fd18ce3d721206ba1e683c2
mutasem-dev <74661061+mutasem-dev@users.noreply.github.com>
2021-10-03 09:39:21 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Main());
+}
+
+class Main extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+            home: Scaffold(
+              appBar: AppBar(
+                title: Text('Student App'),
+                centerTitle: true,
+              ),
+              body: Container(
+                height: 150.0,
+                child: Card(
+                  elevation: 10.0,
+                  color: Colors.white24,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text('Name: ', style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold),),
+                                  Text('Ahmad Tamimi', style: TextStyle(fontSize: 22,letterSpacing: 2.0),),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text('Id: ', style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold),),
+                                  Text('03/1235', style: TextStyle(fontSize: 22,letterSpacing: 2.0),),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                        //Image here
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text('tap to show details'),
+                          Icon(Icons.navigate_next,size: 20,)
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+    );
+  }
+}
+
+

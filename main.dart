a9e0a4e98057d03e28e8399065c36ae2d740a8f5
mutasem-dev <74661061+mutasem-dev@users.noreply.github.com>
2021-10-03 09:45:52 +0200

    Add files via upload

--- main.dart
+++ main.dart
@@ -14,50 +14,56 @@ class Main extends StatelessWidget {
                 title: Text('Student App'),
                 centerTitle: true,
               ),
-              body: Container(
-                height: 150.0,
-                child: Card(
-                  elevation: 10.0,
-                  color: Colors.white24,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              body: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Container(
+                  height: 150.0,
+                  child: Card(
+                    elevation: 10.0,
+                    color: Colors.white24,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text('Name: ', style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold),),
-                                  Text('Ahmad Tamimi', style: TextStyle(fontSize: 22,letterSpacing: 2.0),),
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Text('Id: ', style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold),),
-                                  Text('03/1235', style: TextStyle(fontSize: 22,letterSpacing: 2.0),),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Text('Name: ', style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold),),
+                                      Text('Ahmad Tamimi', style: TextStyle(fontSize: 22,letterSpacing: 2.0),),
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Text('Id: ', style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold),),
+                                      Text('03/1235', style: TextStyle(fontSize: 22,letterSpacing: 2.0),),
+                                    ],
+                                  ),
                                 ],
                               ),
                             ],
+                            //Image here
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text('tap to show details'),
+                              Icon(Icons.navigate_next,size: 20,)
+                            ],
                           ),
                         ],
-                        //Image here
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text('tap to show details'),
-                          Icon(Icons.navigate_next,size: 20,)
-                        ],
-                      ),
-                    ],
+                    ),
                   ),
                 ),
               ),
